72236d2206bbda0439873cd61c88751db33fb946
Sarahi Villegas <sg.villegasq@gmail.com>
2022-01-17 19:09:25 +0000

    Modified the extension HTML to include the text area for the comparison:         - Added a new text area in panel.html         - Loaded that text area in panel.js as comparison
    TODO: compare styles, either through string comparison or by comparing
    the styles object

--- project/extension/js/panel.js
+++ project/extension/js/panel.js
@@ -10,6 +10,7 @@
 		createButton1 = $('#create1'),
 		htmlTextarea1 = $('#html1'),
 		cssTextarea1 = $('#css1'),
+		styles1,
 
 		propertiesCleanUpInput = $('#properties-clean-up'),
 		removeDefaultValuesInput = $('#remove-default-values'),
@@ -19,6 +20,7 @@
 		includeAncestors = $('#include-ancestors'),
 		errorBox = $('#error-box'),
 		loader = $('#loader'),
+		comparison = $('#comparison'),
 		
 		secondSnapshot,
 		cssStringifier2 = new CSSStringifier(),
@@ -29,7 +31,8 @@
 		borderRadiusWorkaround2 = new BorderRadiusWorkaround(),
 		createButton2 = $('#create2'),
 		htmlTextarea2 = $('#html2'),
-		cssTextarea2 = $('#css2')
+		cssTextarea2 = $('#css2'),
+		styles2
 
 	restoreSettings();
 
@@ -42,6 +45,8 @@
 
 	createButton1.on('click', makeFirstSnapshot);
 	createButton2.on('click', makeSecondSnapshot);
+	// compare styles1 and styles2 (the CSS properties)
+	comparison.val("Here goes the comparison of styles!")
 	htmlTextarea1.on('click', function () {
 		$(this).select();
 	});
@@ -193,6 +198,8 @@
 		}
 		console.log(html);
 		htmlTextarea1.val(html);
+
+		styles1 = styles;
 		cssTextarea1.val(cssStringifier1.process(styles));
 
 		loader.removeClass('processing');
@@ -250,6 +257,8 @@
 		}
 
 		htmlTextarea2.val(html);
+
+		styles2 = styles;
 		cssTextarea2.val(cssStringifier2.process(styles));
 
 		loader.removeClass('processing');
