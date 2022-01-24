eb97ed914eb4a658a85f180a4cc80a646ec700ca
Karnbongkot Boonriong <2714363b@student.gla.ac.uk>
2022-01-24 16:05:20 +0000

    Add allowed values for classes attributes

---
+++ project/extension/js/values.js
@@ -0,0 +1,55 @@
+const FONT_STYLE = {
+    //The browser displays a normal font style. This is default
+    Normal : "normal",
+    //The browser displays an italic font style
+    Italic : "italic",
+    //The browser displays an oblique font style
+    Oblique : "oblique",
+    //Sets this property to its default value
+    Initial : "initial",
+    //Inherits this property from its parent element
+    Inherit : "inherit"
+};
+const FONT_VARIANT = {
+    Normal : "normal",
+    //The browser displays a normal font. This is default
+    Small_Caps : "small caps",
+    //The browser displays a small-caps font
+    Initial : "initial",
+    //Sets this property to its default value. Read about initial
+    Inherit : "inherit"
+    //Inherits this property from its parent element.
+};
+
+const FONT_WEIGHT = {
+    Normal : "normal",
+    //Defines normal characters. This is default
+    Bold : "bold",
+    //Defines thick characters
+    Bolder : "bolder",
+    //Defines thicker characters
+    Lighter : "lighter",
+    //Defines lighter characters
+    100 : "100",
+    200 : "200",
+    300 : "300",
+    400 : "400",
+    500 : "500",
+    600 : "600",
+    700 : "700",
+    800 : "800",
+    900 : "900",
+    //Defines from thin to thick characters. 400 is the same as normal, and 700 is the same as bold
+    Initial : "initial",
+    //Sets this property to its default value. Read about initial
+    Inherit : "inherit"
+    //Inherits this property from its parent element.
+};
+
+const  GENERIC_FAMILY = {
+    Serif : "serif",
+    Sans_Serif : "sans-serif",
+    Cursive : "cursive",
+    Fantasy : "fantasy",
+    Monospace : "monospace"
+};
