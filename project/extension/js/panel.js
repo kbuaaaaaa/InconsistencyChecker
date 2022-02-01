add2b419e7fd4c127024065b1276a52642daa76f
Karnbongkot Boonriong <2714363b@student.gla.ac.uk>
2022-02-01 19:34:15 +0000

    Added input generator for all the different properties of the template (kill me pls. I want to be dead)

--- project/extension/js/panel.js
+++ project/extension/js/panel.js
@@ -74,6 +74,7 @@
 
 	data.index = 0;
 	data.list = [
+		"select",
 		"color",
 		"font",
 		"border",
@@ -348,6 +349,8 @@
 		var select_label = document.createElement("label");
 		select_label.innerHTML = " Select Property ";
 
+		var property_value_div = document.createElement("div");
+
 		var select = document.createElement("select");
 		select.className = "select_property";
 		for (const val of data.list){
@@ -357,37 +360,250 @@
 			select.appendChild(option);
     	}
 		select.style = "margin-left: 20px;";
-		var value = select.options[select.selectedIndex].value;
-		switch (value) {
-			case "color":
-				break;
-			case "font":
-				break;
-			case "border":
-				break;
-			case "width":
-				var property_label = document.createElement("label");
-				property_label.innerHTML = " Property Value ";
-				property_label.style = "margin-left: 20px;";
-		
-				var property_input = document.createElement("input");
-				property_input.type = "text";
-				property_input.className = "property_value";
-				property_input.style = "margin-left: 20px;";
-				break;
-			case "height":
-				var property_label = document.createElement("label");
-				property_label.innerHTML = " Property Value ";
-				property_label.style = "margin-left: 20px;";
-		
-				var property_input = document.createElement("input");
-				property_input.type = "text";
-				property_input.className = "property_value";
-				property_input.style = "margin-left: 20px;";
-				break;
-			default:
-				break;
-		}
+		select.onchange = function(){
+			var value = select.options[select.selectedIndex].value;
+			if(property_value_div.childElementCount > 0){
+				property_value_div.removeChild(property_value_div.lastChild);
+			}
+			switch (value) {
+				case "color":
+					var color_div = document.createElement("div");
+					color_div.className = "color_div";
+
+					var red_label = document.createElement("label");
+					red_label.innerHTML = " R ";
+					red_label.style = "margin-left: 20px;";
+					color_div.appendChild(red_label);
+			
+					var red_input = document.createElement("input");
+					red_input.type = "text";
+					red_input.className = "red_value";
+					red_input.style = "margin-left: 20px;";
+					color_div.appendChild(red_input);
+
+					var green_label = document.createElement("label");
+					green_label.innerHTML = " G ";
+					green_label.style = "margin-left: 20px;";
+					color_div.appendChild(green_label);
+			
+					var green_input = document.createElement("input");
+					green_input.type = "text";
+					green_input.className = "green_value";
+					green_input.style = "margin-left: 20px;";
+					color_div.appendChild(green_input);
+
+					var blue_label = document.createElement("label");
+					blue_label.innerHTML = " B ";
+					blue_label.style = "margin-left: 20px;";
+					color_div.appendChild(blue_label);
+			
+					var blue_input = document.createElement("input");
+					blue_input.type = "text";
+					blue_input.className = "blue_value";
+					blue_input.style = "margin-left: 20px;";
+					color_div.appendChild(blue_input);
+
+					property_value_div.appendChild(color_div)
+					break;
+				case "font":
+					var font_div = document.createElement("div");
+					font_div.className = "font_div";
+
+					var font_style_label = document.createElement("label");
+					font_style_label.innerHTML = " Font Style ";
+					font_style_label.style = "margin-left: 20px;";
+					font_div.appendChild(font_style_label);
+
+					var font_style_input = document.createElement("select");
+					font_style_input.className = "font_style_input";
+					for (const key of Object.keys(FONT_STYLE)){
+						var option = document.createElement("option");
+						option.value = key;
+						option.text = key.charAt(0).toUpperCase() + key.slice(1);
+						font_style_input.appendChild(option);
+					}
+					font_style_input.style = "margin-left: 20px;";
+					font_div.appendChild(font_style_input);
+
+					var font_variant_label = document.createElement("label");
+					font_variant_label.innerHTML = " Font Variant ";
+					font_variant_label.style = "margin-left: 20px;";
+					font_div.appendChild(font_variant_label);
+
+					var font_variant_input = document.createElement("select");
+					font_variant_input.className = "font_variant_input";
+					for (const key of Object.keys(FONT_VARIANT)){
+						var option = document.createElement("option");
+						option.value = key;
+						option.text = key.charAt(0).toUpperCase() + key.slice(1);
+						font_variant_input.appendChild(option);
+					}
+					font_variant_input.style = "margin-left: 20px;";
+					font_div.appendChild(font_variant_input);
+
+					var font_weight_label = document.createElement("label");
+					font_weight_label.innerHTML = " Font Weight ";
+					font_weight_label.style = "margin-left: 20px;";
+					font_div.appendChild(font_weight_label);
+
+					var font_weight_input = document.createElement("select");
+					font_weight_input.className = "font_weight_input";
+					for (const key of Object.keys(FONT_WEIGHT)){
+						var option = document.createElement("option");
+						option.value = key;
+						option.text = key.charAt(0).toUpperCase() + key.slice(1);
+						font_weight_input.appendChild(option);
+					}
+					font_weight_input.style = "margin-left: 20px;";
+					font_div.appendChild(font_weight_input);
+
+					var font_size_label = document.createElement("label");
+					font_size_label.innerHTML = " Font Size ";
+					font_size_label.style = "margin-left: 20px;";
+					font_div.appendChild(font_size_label);
+
+					var font_size_input = document.createElement("input");
+					font_size_input.type = "text";
+					font_size_input.className = "font_size_value";
+					font_size_input.style = "margin-left: 20px;";
+					font_div.appendChild(font_size_input);
+
+					var line_height_label = document.createElement("label");
+					line_height_label.innerHTML = " Line Height ";
+					line_height_label.style = "margin-left: 20px;";
+					font_div.appendChild(line_height_label);
+
+					var line_height_input = document.createElement("input");
+					line_height_input.type = "text";
+					line_height_input.className = "line_height_value";
+					line_height_input.style = "margin-left: 20px;";
+					font_div.appendChild(line_height_input);
+
+					var font_name_label = document.createElement("label");
+					font_name_label.innerHTML = " Font Name ";
+					font_name_label.style = "margin-left: 20px;";
+					font_div.appendChild(font_name_label);
+
+					var font_name_input = document.createElement("input");
+					font_name_input.type = "text";
+					font_name_input.className = "font_name_value";
+					font_name_input.style = "margin-left: 20px;";
+					font_div.appendChild(font_name_input);
+
+					var font_family_label = document.createElement("label");
+					font_family_label.innerHTML = " Font Family ";
+					font_family_label.style = "margin-left: 20px;";
+					font_div.appendChild(font_family_label);
+
+					var font_family_input = document.createElement("input");
+					font_family_input.type = "text";
+					font_family_input.className = "font_family_value";
+					font_family_input.style = "margin-left: 20px;";
+					font_div.appendChild(font_family_input);
+
+					var generic_family_label = document.createElement("label");
+					generic_family_label.innerHTML = " Generic Family ";
+					generic_family_label.style = "margin-left: 20px;";
+					font_div.appendChild(generic_family_label);
+
+					var generic_family_input = document.createElement("select");
+					generic_family_input.className = "generic_family_input";
+					for (const key of Object.keys(FONT_WEIGHT)){
+						var option = document.createElement("option");
+						option.value = key;
+						option.text = key.charAt(0).toUpperCase() + key.slice(1);
+						generic_family_input.appendChild(option);
+					}
+					generic_family_input.style = "margin-left: 20px;";
+					font_div.appendChild(generic_family_input);
+
+
+					property_value_div.appendChild(font_div);
+					break;
+				case "border":
+					var border_div = document.createElement("div");
+					border_div.className = "border_div";
+
+					var border_width_label = document.createElement("label");
+					border_width_label.innerHTML = " Border Width ";
+					border_width_label.style = "margin-left: 20px;";
+					border_div.appendChild(border_width_label);
+
+					var border_width_input = document.createElement("input");
+					border_width_input.type = "text";
+					border_width_input.className = "border_width_value";
+					border_width_input.style = "margin-left: 20px;";
+					border_div.appendChild(_input);
+
+					var border_style_label = document.createElement("label");
+					border_style_label.innerHTML = " Border Style ";
+					border_style_label.style = "margin-left: 20px;";
+					border_div.appendChild(_label);
+
+					var border_style_input = document.createElement("select");
+					border_style_input.className = "border_style_input";
+					for (const key of Object.keys(BORDER_STYLE)){
+						var option = document.createElement("option");
+						option.value = key;
+						option.text = key.charAt(0).toUpperCase() + key.slice(1);
+						border_style_input.appendChild(option);
+					}
+					border_style_input.style = "margin-left: 20px;";
+					border_div.appendChild(font_style_input);
+
+					var border_color_label = document.createElement("label");
+					border_color_label.innerHTML = " Border Color ";
+					border_color_label.style = "margin-left: 20px;";
+					border_div.appendChild(_label);
+
+					var border_color_input = document.createElement("input");
+					border_color_input.type = "text";
+					border_color_input.className = "border_color_value";
+					border_color_input.style = "margin-left: 20px;";
+					border_div.appendChild(_input);
+
+
+					property_value_div.appendChild(border_div);
+					break;
+				case "width":
+					var width_div = document.createElement("div");
+					width_div.className = "width_div";
+
+					var width_label = document.createElement("label");
+					width_label.innerHTML = " Value ";
+					width_label.style = "margin-left: 20px;";
+					width_div.appendChild(width_label);
+			
+					var width_input = document.createElement("input");
+					width_input.type = "text";
+					width_input.className = "width_value";
+					width_input.style = "margin-left: 20px;";
+					width_div.appendChild(width_input);
+
+					property_value_div.appendChild(width_div);
+					break;
+				case "height":
+					var height_div = document.createElement("div");
+					height_div.className = "height_div";
+
+					var height_label = document.createElement("label");
+					height_label.innerHTML = " Value ";
+					height_label.style = "margin-left: 20px;";
+					height_div.appendChild(height_label);
+			
+					var height_input = document.createElement("input");
+					height_input.type = "text";
+					height_input.className = "height_value";
+					height_input.style = "margin-left: 20px;";
+					height_div.appendChild(height_input);
+
+					property_value_div.appendChild(height_div);
+					break;
+				default:
+					break;
+			}
+		};
+
 
 		var delete_button = document.createElement("button");
 		delete_button.innerHTML = " Delete ";
@@ -396,25 +612,14 @@
 
 		div.appendChild(select_label);
 		div.appendChild(select);
-		div.appendChild(property_label);
-		div.appendChild(property_input);
+		div.appendChild(property_value_div);
 		div.appendChild(delete_button);
 		var property_div = document.getElementById("property_div");
 		property_div.appendChild(div);
     }
 
     function save(){
-        for (let i = 0; i < data.list.length; i++) {
-            const element = data.list[i];
-            document.getElementById('template_div').css({ element: "" })
-        }
-		document.getElementsByClassName("select_div").each(function (index, domEle) {
-            var select_property = (domEle).find('.select_property').val().toString();
-            var property_value = (domEle).find('.property_value').val().toString();
-            if(property_value!=null&& property_value!=undefined&& property_value!=''){
-                document.getElementById('#template_div').css({ select_property : property_value})
-            }
-        });
+
     }
 })();
 
