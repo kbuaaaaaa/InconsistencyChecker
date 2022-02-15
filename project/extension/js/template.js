c94293c85eafc6b8510a4d5d91dc3836f6d61840
Karnbongkot Boonriong <2714363b@student.gla.ac.uk>
2022-02-15 17:30:21 +0000

    Define compare function in template class

--- project/extension/js/template.js
+++ project/extension/js/template.js
@@ -4,6 +4,89 @@ class Template{
         this.font = font;
         this.border = border;
     }
+
+    compare(element){
+      let fontResult = PROPERTY.None,
+          borderResult = PROPERTY.None,
+          colorResult = PROPERTY.None
+      
+      for (const font of this.font) {
+        let match = true;
+        if (FONT_STYLE[font.font_style] !== element.font.font_style) {
+          match = false;
+        }
+        if (FONT_VARIANT[font.font_variant] !== element.font.font_variant) {
+          match = false;
+        }
+        if (FONT_WEIGHT[font.font_weight] !== element.font.font_weight) {
+          match = false;
+        }
+        if (font.font_size !== "") {
+          if (font.font_size !== element.font.font_size) {
+            match = false;
+          }
+        }
+        if (font.line_height !== "") {
+          if (font.line_height !== element.font.line_height) {
+            match = false;
+          }
+        }
+        if (font.font_name !== "") {
+          if (font.font_name !== element.font.font_name) {
+            match = false;
+          }
+        }
+        if (font.font_family !== "") {
+          if (font.font_family !== element.font.font_family) {
+            match = false;
+          }
+        }
+        if (GENERIC_FAMILY[font.generic_family] !== element.font.generic_family) {
+          match = false;
+        }
+        if(match){
+          fontResult = PROPERTY.Consistent;
+          break;
+        }else{
+          fontResult = PROPERTY.Inconsistent;
+        }
+      }
+
+      for (const border of this.border) {
+        let match = true;
+        if (BORDER_STYLE[border.border_style] !== element.border.border_style) {
+          match = false;
+        }
+        if (border.border_width !== "") {
+          if (border.border_width !== element.border.border_width) {
+            match = false;
+          }
+        }
+        if (border.border_color !== "") {
+          if (border.border_color !== element.border.border_color) {
+            match = false;
+          }
+        }
+        if(match){
+          borderResult = PROPERTY.Consistent;
+          break;
+        }else{
+          borderResult = PROPERTY.Inconsistent;
+        }
+      }
+
+      for (const color of this.color) {
+        if (color.color !== element.color.color) {
+          colorResult = PROPERTY.Inconsistent
+        }else{
+          colorResult = PROPERTY.Inconsistent
+          break;
+        }
+      }
+
+      return {fontResult, colorResult, borderResult};
+    }
+
 }
 
 class Element{
@@ -34,6 +117,10 @@ class Font {
     this.font_name = font_name;
     this.font_family = font_family;
     this.generic_family = generic_family;
+  }
+
+  toString(){
+    
   }
 }
 
