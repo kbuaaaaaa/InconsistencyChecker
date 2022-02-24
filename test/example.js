1d903383849fec704f0627a5cdb4d5db3ac8dbd9
Karnbongkot Boonriong <2714363b@student.gla.ac.uk>
2022-02-24 18:47:38 +0000

    Update example test

--- test/example.js
+++ test/example.js
@@ -1,9 +1,40 @@
 var test = require('unit.js');
-// just for example of tested value
-var example = 'hello';
-// assert that example variable is a string
-test.string(example);
-// or with Must.js
-test.must(example).be.a.string();
-// or with assert
-test.assert(typeof example === 'string');
+describe('Learning by the example', function(){
+  it('example variable', function(){
+    // just for example of tested value
+    var example = 'hello world';
+    test
+      .string(example)
+        .startsWith('hello')
+        .match(/[a-z]/)
+      .given(example = 'you are welcome')
+        .string(example)
+          .endsWith('welcome')
+          .contains('you')
+      .when('"example" becomes an object', function(){
+        example = {
+          message: 'hello world',
+          name: 'Nico',
+          job: 'developper',
+          from: 'France'
+        };
+      })
+      .then('test the "example" object', function(){
+        test
+          .object(example)
+            .hasValue('developper')
+            .hasProperty('name')
+            .hasProperty('from', 'France')
+            .contains({message: 'hello world'})
+        ;
+      })
+      .if(example = 'bad value')
+        .error(function(){
+          example.badMethod();
+        })
+    ;
+  });
+  it('other test case', function(){
+    // other tests ...
+  });
+});
