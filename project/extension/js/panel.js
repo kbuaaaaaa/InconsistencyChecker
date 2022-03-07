5ee44a8eb7952b8383cb0a84032084524b8216b3
Alex Tonca <2437707t@student.gla.ac.uk>
2022-03-07 19:26:35 +0000

    Fixes minor bug

--- project/extension/js/panel.js
+++ project/extension/js/panel.js
@@ -141,42 +141,6 @@
         });
         reader.readAsText(this.files[0]);
       });
-    document
-      .querySelector("#file-selector-template-page")
-      .addEventListener("change", function () {
-        const reader = new FileReader();
-        reader.addEventListener("load", () => {
-          localStorage.setItem("json-file", reader.result);
-          var styleFromJSON = JSON.parse(reader.result);
-          var templateParsed = new Template([], [], []);
-          for (const font of styleFromJSON.font) {
-            var temp = new Font(
-              font.font_style,
-              font.font_variant,
-              font.font_weight,
-              font.font_size,
-              font.line_height,
-              font.font_family
-            );
-            templateParsed.font.push(temp);
-          }
-          for (const border of styleFromJSON.border) {
-            var temp = new Border(
-              border.border_width,
-              border.border_style,
-              border.border_color
-            );
-            templateParsed.border.push(temp);
-          }
-          for (const color of styleFromJSON.color) {
-            var temp = new Color(color.color);
-            templateParsed.color.push(temp);
-          }
-
-          template = templateParsed;
-        });
-        reader.readAsText(this.files[0]);
-      });
   }
   readTemplate();
 
