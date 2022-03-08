ad725e306b0a5b5e88a5ed2667c732813087aba0
Karnbongkot Boonriong <2714363b@student.gla.ac.uk>
2022-03-08 17:24:49 +0000

    Create Test Initializer - There are many variable that has to be import before test - Create a function that does initialization and take callback

---
+++ test/test_initializer.js
@@ -0,0 +1,24 @@
+const jsdom = require('jsdom');
+function TestInitializer(_callback){
+    return jsdom.JSDOM.fromFile('project/extension/panel.html')
+      .then(function(dom) {
+        var window = dom.window;
+        const jquery = require("jquery")(dom.window);
+        global.document = window.document;
+        global.window = window;
+        var $ = jQuery = require('jquery')(window);
+        global.$ = $;
+      }).then(function() {
+        global.CSSStringifier = require( "../project/extension/js/tools/CSSStringifier").CSSStringifier;
+        global.ShorthandPropertyFilter = require( "../project/extension/js/filters/ShorthandPropertyFilter").ShorthandPropertyFilter;
+        global.WebkitPropertiesFilter = require( "../project/extension/js/filters/WebkitPropertiesFilter").WebkitPropertiesFilter;
+        global.DefaultValueFilter = require( "../project/extension/js/filters/DefaultValueFilter").DefaultValueFilter;
+        global.SameRulesCombiner = require( "../project/extension/js/tools/SameRulesCombiner").SameRulesCombiner;
+        global.BorderRadiusWorkaround = require( "../project/extension/js/tools/BorderRadiusWorkaround").BorderRadiusWorkaround;
+        global.Snapshooter = require("../project/extension/js/tools/Snapshooter.js").Snapshooter;
+        global.diff_match_patch = require("../project/extension/js/diff_match_patch.js").diff_match_patch;
+        _callback();
+      });
+}
+
+module.exports = {TestInitializer};
