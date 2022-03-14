a49aa15e879a9a419dbc50db07ca0e20b6a3419a
Karnbongkot Boonriong <2714363b@student.gla.ac.uk>
2022-03-14 01:17:27 +0000

    Update test initializer

--- test/test_initializer.js
+++ test/test_initializer.js
@@ -1,6 +1,30 @@
 const jsdom = require('jsdom');
-function TestInitializer(_callback){
-    return jsdom.JSDOM.fromFile('project/extension/panel.html')
+global.FONT_STYLE = require("../project/extension/js/values.js").FONT_STYLE;
+global.FONT_VARIANT = require("../project/extension/js/values.js").FONT_VARIANT;
+global.FONT_WEIGHT = require("../project/extension/js/values.js").FONT_WEIGHT;
+global.GENERIC_FAMILY = require("../project/extension/js/values.js").GENERIC_FAMILY;
+global.BORDER_STYLE = require("../project/extension/js/values.js").BORDER_STYLE;
+global.PROPERTY = require("../project/extension/js/values.js").PROPERTY;
+global.Template = require("../project/extension/js/template.js").Template;
+global.Element = require("../project/extension/js/template.js").Element;
+global.Color = require("../project/extension/js/template.js").Color;
+global.Border = require("../project/extension/js/template.js").Border;
+global.Font = require("../project/extension/js/template.js").Font;
+global.CSSStringifier = require( "../project/extension/js/tools/CSSStringifier").CSSStringifier;
+global.ShorthandPropertyFilter = require( "../project/extension/js/filters/ShorthandPropertyFilter").ShorthandPropertyFilter;
+global.WebkitPropertiesFilter = require( "../project/extension/js/filters/WebkitPropertiesFilter").WebkitPropertiesFilter;
+global.DefaultValueFilter = require( "../project/extension/js/filters/DefaultValueFilter").DefaultValueFilter;
+global.SameRulesCombiner = require( "../project/extension/js/tools/SameRulesCombiner").SameRulesCombiner;
+global.BorderRadiusWorkaround = require( "../project/extension/js/tools/BorderRadiusWorkaround").BorderRadiusWorkaround;
+global.Snapshooter = require("../project/extension/js/tools/Snapshooter.js").Snapshooter;
+global.diff_match_patch = require("../project/extension/js/diff_match_patch.js").diff_match_patch;
+global.template = new Template("test",[new Color("#ffffff")],[new Font("normal","normal","400",14,20,"\"Amazon Ember\", Arial, sans-serif")],[new Border(10,"none","#ffffff")]);
+global.switch = 1;
+global.firstSnapshot = require("./firstElement.json");
+global.secondSnapshot = require("./secondElement.json");
+
+function TestInitializer(pageName,_callback){
+    return jsdom.JSDOM.fromFile(`project/extension/${pageName}.html`)
       .then(function(dom) {
         var window = dom.window;
         const jquery = require("jquery")(dom.window);
@@ -9,14 +33,7 @@ function TestInitializer(_callback){
         var $ = jQuery = require('jquery')(window);
         global.$ = $;
       }).then(function() {
-        global.CSSStringifier = require( "../project/extension/js/tools/CSSStringifier").CSSStringifier;
-        global.ShorthandPropertyFilter = require( "../project/extension/js/filters/ShorthandPropertyFilter").ShorthandPropertyFilter;
-        global.WebkitPropertiesFilter = require( "../project/extension/js/filters/WebkitPropertiesFilter").WebkitPropertiesFilter;
-        global.DefaultValueFilter = require( "../project/extension/js/filters/DefaultValueFilter").DefaultValueFilter;
-        global.SameRulesCombiner = require( "../project/extension/js/tools/SameRulesCombiner").SameRulesCombiner;
-        global.BorderRadiusWorkaround = require( "../project/extension/js/tools/BorderRadiusWorkaround").BorderRadiusWorkaround;
-        global.Snapshooter = require("../project/extension/js/tools/Snapshooter.js").Snapshooter;
-        global.diff_match_patch = require("../project/extension/js/diff_match_patch.js").diff_match_patch;
+        global.chrome = null;
         _callback();
       });
 }
