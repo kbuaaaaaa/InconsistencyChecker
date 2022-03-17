f0ded8ef2cbe8dc345e043c0163c40e1b2c4db74
Sarahi Gabriela Villegas Quinonez <2477179V@student.gla.ac.uk>
2022-03-17 21:52:04 +0000

    Run linting and fix errors on global and panel
    See #90

--- project/extension/js/global.js
+++ project/extension/js/global.js
@@ -1,43 +1,43 @@
-var data = {};
+const data = {};
 data.index = 0;
-data.list = ["select", "color", "font", "border"];
+data.list = ['select', 'color', 'font', 'border'];
 
-function readTemplate(reader,_callback) {
-    reader.addEventListener("load", () => {
-      localStorage.setItem("template",reader.result);
-      if(_callback){
-        _callback();
-      }
-    });
+function readTemplate(reader, _callback) {
+  reader.addEventListener('load', () => {
+    localStorage.setItem('template', reader.result);
+    if (_callback) {
+      _callback();
+    }
+  });
 }
 
-function getTemplate(){
-  let template = JSON.parse(localStorage.getItem("template"));
-  var templateParsed = new Template();
+function getTemplate() {
+  let template = JSON.parse(localStorage.getItem('template'));
+  const templateParsed = new Template();
   templateParsed.name = template.name;
   for (const font of template.font) {
-    var temp = new Font(
+    const temp = new Font(
       font.font_style,
       font.font_variant,
       font.font_weight,
       font.font_size,
       font.line_height,
-      font.font_family
+      font.font_family,
     );
     templateParsed.font.push(temp);
   }
 
   for (const border of template.border) {
-    var temp = new Border(
+    const temp = new Border(
       border.border_width,
       border.border_style,
-      border.border_color
+      border.border_color,
     );
     templateParsed.border.push(temp);
   }
 
   for (const color of template.color) {
-    var temp = new Color(color.color);
+    const temp = new Color(color.color);
     templateParsed.color.push(temp);
   }
 
@@ -45,8 +45,10 @@ function getTemplate(){
   return template;
 }
 
-function storeTemplate(template){
-  localStorage.setItem("template",JSON.stringify(template, null, 2));
+function storeTemplate(template) {
+  localStorage.setItem('template', JSON.stringify(template, null, 2));
 }
 
-if (typeof module !== 'undefined'){module.exports = {readTemplate,getTemplate,storeTemplate};};
+if (typeof module !== 'undefined') {
+  module.exports = { readTemplate, getTemplate, storeTemplate };
+}
